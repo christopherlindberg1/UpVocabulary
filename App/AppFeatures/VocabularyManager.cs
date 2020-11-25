ef0743a5ff7fb30c30eaa6eebadfc6816d8f1cf0
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-11-26 00:13:47 +0100

    Added methods for getting vocabulary by name and index, as well as adding vocabulary

--- App/AppFeatures/VocabularyManager.cs
+++ App/AppFeatures/VocabularyManager.cs
@@ -6,7 +6,7 @@ namespace AppFeatures
 {
     public class VocabularyManager
     {
-        private readonly List<Vocabulary> _vocabularies;
+        private readonly List<Vocabulary> _vocabularies = new List<Vocabulary>();
 
 
 
@@ -22,15 +22,20 @@ namespace AppFeatures
         {
             get => _vocabularies;
 
-            set
-            {
-                if (value == null)
-                {
-                    throw new ArgumentNullException(
-                        "Vocabularies",
-                        "Vocabularies cannot be null");
-                }
-            }
+            //set
+            //{
+            //    if (value == null)
+            //    {
+            //        throw new ArgumentNullException(
+            //            "Vocabularies",
+            //            "Vocabularies cannot be null");
+            //    }
+            //}
+        }
+
+        public int NrOfVocabularies
+        {
+            get => Vocabularies.Count;
         }
 
 
@@ -45,7 +50,7 @@ namespace AppFeatures
 
         public VocabularyManager()
         {
-            Vocabularies = new List<Vocabulary>();
+            //Vocabularies = new List<Vocabulary>();
         }
 
 
@@ -58,9 +63,47 @@ namespace AppFeatures
          * 
          */
 
-        private void FillWithSampleData()
+        public Vocabulary GetAtIndex(int index)
         {
+            if (index < 0)
+            {
+                return null;
+            }
 
+            if (index > Vocabularies.Count - 1)
+            {
+                return null;
+            }
+                
+            return Vocabularies[index];
         }
+
+        public Vocabulary GetVocabulary(string name)
+        {
+            for (int i = 0; i < Vocabularies.Count; i++)
+            {
+                if (String.Compare(Vocabularies[i].Name, name, true) == 0)
+                {
+                    return Vocabularies[i];
+                }
+            }
+
+            return null;
+        }
+
+        public bool AddVocabulary(Vocabulary vocabulary)
+        {
+            Vocabulary existingVocabulary = GetVocabulary(vocabulary.Name);
+
+            if (existingVocabulary != null)
+            {
+                return false;
+            }
+
+            Vocabularies.Add(vocabulary);
+            return true;
+        }
+
+        
     }
 }
