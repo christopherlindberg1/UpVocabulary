124498c3836b9c6391cd3b42716619a68cdf9e66
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-11-25 13:16:28 +0100

    Added some fields and a property

--- App/AppFeatures/Vocabulary.cs
+++ App/AppFeatures/Vocabulary.cs
@@ -6,5 +6,36 @@ namespace AppFeatures
 {
     public class Vocabulary
     {
+        private string _name;
+        private readonly string[] _languages = new string[2];
+        private readonly List<Word> _words;
+
+
+
+
+        /**
+         * 
+         * ===================  Properties  ===================
+         * 
+         */
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Name", "Name cannot be null");
+                }
+
+                _name = value;
+            }
+        }
+
+
     }
 }
