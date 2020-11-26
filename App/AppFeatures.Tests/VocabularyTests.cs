3fdc9f11e11ed3e545776630b94082e82299052b
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-11-26 16:33:37 +0100

    Initialized SampleVocabulary with sample data

--- App/AppFeatures.Tests/VocabularyTests.cs
+++ App/AppFeatures.Tests/VocabularyTests.cs
@@ -2,53 +2,68 @@
 using System.Collections.Generic;
 using System.Text;
 using AppFeatures;
+using Xunit;
 
 namespace AppFeatures.Tests
 {
     public class VocabularyTests
     {
-        private Vocabulary _sampleVocabulary;
+        /// <summary>
+        ///   Vocabulary field initialized with sample data for all fields. 3 words in total.
+        /// </summary>
+        private readonly Vocabulary _sampleVocabulary =
+            new Vocabulary(
+                "D is for Digital",
+                new List<Word>
+                {
+                    new Word
+                    {
+                        OriginalWord = "b",
+                        Translation = "b",
+                        Sentence = "b b b",
+                        Weight = 5,
+                        TimesAnsweredCorrectly = 0
+                    },
+                    new Word
+                    {
+                        OriginalWord = "c",
+                        Translation = "c",
+                        Sentence = "Ccc!",
+                        Weight = 5,
+                        TimesAnsweredCorrectly = 0
+                    },
+                    new Word
+                    {
+                        OriginalWord = "e",
+                        Translation = "eeeeeee",
+                        Sentence = "Eeee ee ee eee",
+                        Weight = 5,
+                        TimesAnsweredCorrectly = 0
+                    }
+
+                },
+                "English",
+                "Swedish");
         
         private Vocabulary SampleVocabulary
         {
             get => _sampleVocabulary;
-
-            set => _sampleVocabulary = value ??
-                throw new ArgumentNullException(
-                    "SampleVocabulary",
-                    "SampleVocabulary cannot be null.");
         }
 
 
-        private void FillSampleVocabularyWithData()
-        {
-            Word w1 = new Word
-            {
-                OriginalWord = "b",
-                Translation = "b",
-                Sentence = "b b b",
-                Weight = 5,
-                TimesAnsweredCorrectly = 0
-            };
-            Word w2 = new Word
-            {
-                OriginalWord = "c",
-                Translation = "c",
-                Sentence = "Ccc!",
-                Weight = 5,
-                TimesAnsweredCorrectly = 0
-            };
-            Word w3 = new Word
-            {
-                OriginalWord = "e",
-                Translation = "eeeeeee",
-                Sentence = "Eeee ee ee eee",
-                Weight = 5,
-                TimesAnsweredCorrectly = 0
-            };
+        
 
-            List<Word> sampleWords = new List<Word> { w1, w2, w3 };
-            SampleVocabulary = new Vocabulary("D is for Digital", sampleWords, "English", "Swedish");
+        [Fact]
+        public void Test()
+        {
+            // Arrange
+            bool expected = true;
+
+            // Act
+            bool actual = true;
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
