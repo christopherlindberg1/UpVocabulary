770ceb2043be4469f06a83d0eb9bbbbd29676597
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-11-28 00:25:14 +0100

    Added three methods for testing the different scenarios of the RemoveWord method

--- App/AppFeatures.Tests/VocabularyTests.cs
+++ App/AppFeatures.Tests/VocabularyTests.cs
@@ -14,45 +14,75 @@ namespace AppFeatures.Tests
         private static readonly List<Word> _sampleWords = new List<Word>
         {
             new Word
-            {
-                OriginalWord = "b",
-                Translation = "b",
-                Sentence = "b b b",
-                Weight = 5,
-                TimesAnsweredCorrectly = 0
-            },
+            (
+                "b",
+                "b",
+                "b b b",
+                5,
+                0
+            ),
             new Word
-            {
-                OriginalWord = "c",
-                Translation = "c",
-                Sentence = "Ccc!",
-                Weight = 5,
-                TimesAnsweredCorrectly = 0
-            },
+            (
+                "c",
+                "c",
+                "Ccc!",
+                5,
+                0
+            ),
             new Word
-            {
-                OriginalWord = "e",
-                Translation = "eeeeeee",
-                Sentence = "Eeee ee ee eee",
-                Weight = 5,
-                TimesAnsweredCorrectly = 0
-            }
+            (
+                "e",
+                "eeeeeee",
+                "Eeee ee ee eee",
+                5,
+                0
+            )
         };
 
         /// <summary>
         ///   Vocabulary initialized with sample data for all its fields. 3 words in total.
         /// </summary>
-        private static readonly Vocabulary _sampleVocabulary = new Vocabulary
+        private static Vocabulary _sampleVocabulary = new Vocabulary
             (
                 "D is for Digital",
                 SampleWords,
                 "English",
                 "Swedish"
             );
-        
+
+        private static Vocabulary _emptySampleVocabulary = new Vocabulary
+            (
+                "D is for Digital",
+                new List<Word>(),
+                "English",
+                "Swedish"
+            );
+
+
+
+
+        /**
+         * 
+         * ===================  Properties  ===================
+         * 
+         */
+
         private static Vocabulary SampleVocabulary
         {
             get => _sampleVocabulary;
+
+            set
+            {
+                _sampleVocabulary = value
+                    ?? throw new ArgumentNullException(
+                        "SampleVocabulary",
+                        "SampleVocabulary cannot be null");
+            }
+        }
+
+        private static Vocabulary EmptySampleVocabulary
+        {
+            get => _emptySampleVocabulary;
         }
 
         private static List<Word> SampleWords
@@ -60,6 +90,14 @@ namespace AppFeatures.Tests
             get => _sampleWords;
         }
 
+
+
+
+        /**
+         * 
+         * ===================  Tests  ===================
+         * 
+         */
 
         [Fact]
         public void GetWord_ShouldReturnFirstWord()
@@ -99,6 +137,70 @@ namespace AppFeatures.Tests
 
             // Assert
             Assert.Null(actual);
+        }
+
+        [Theory]
+        [InlineData("b")]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void RemoveWord_ShouldWorkWhenEmpty(string word)
+        {
+            // Arrange
+            bool expectedReturnValue = false;
+            int expectedNrOfWordsLeft = 0;
+            Word expectedGetWordAfterRemoval = null;
+
+            // Act
+            bool actualReturnValue = EmptySampleVocabulary.RemoveWord(word);
+            int actualNrOfWordsLeft = EmptySampleVocabulary.NrOfWords;
+            Word actualGetWordAfterRemoval = EmptySampleVocabulary.GetWord(word);
+
+            // Assert
+            Assert.Equal(expectedReturnValue, actualReturnValue);
+            Assert.Equal(expectedNrOfWordsLeft, actualNrOfWordsLeft);
+            Assert.Equal(expectedGetWordAfterRemoval, actualGetWordAfterRemoval);
+        }
+
+        [Fact]
+        public void RemoveWord_ShouldWorkWhenNotEmpty()
+        {
+            // Arrange
+            Word word = new Word("hello", "hej", null, 5, 0);
+            bool expectedReturnValue = true;
+            Word expectedWordToGetAfterDelete = null;
+            int expectedNrOfWords = SampleVocabulary.NrOfWords;
+
+            // Act
+            SampleVocabulary.InsertWord(word);
+            bool actualReturnValue = SampleVocabulary.RemoveWord(word.OriginalWord);
+            Word actualWordAfterDelete = SampleVocabulary.GetWord(word.OriginalWord);
+            int actualNrOfWords = SampleVocabulary.NrOfWords;
+
+            // Assert
+            Assert.Equal(expectedReturnValue, actualReturnValue);
+            Assert.Equal(expectedWordToGetAfterDelete, actualWordAfterDelete);
+            Assert.Equal(expectedNrOfWords, actualNrOfWords);
+        }
+
+        [Fact]
+        public void RemoveWord_ShouldReturnFalseWhenWordDoesNotExist()
+        {
+            // Arrange
+            Word word = new Word("hello", "hej", null, 5, 0);
+            bool expectedReturnValue = false;
+            Word expectedWordToGetAfterDelete = null;
+            int expectedNrOfWords = SampleVocabulary.NrOfWords;
+
+            // Act
+            bool actualReturnValue = SampleVocabulary.RemoveWord(word.OriginalWord);
+            Word ActualWordAfterDelete = SampleVocabulary.GetWord(word.OriginalWord);
+            int actualNrOfWords = SampleVocabulary.NrOfWords;
+
+            // Assert
+            Assert.Equal(expectedReturnValue, actualReturnValue);
+            Assert.Equal(expectedWordToGetAfterDelete, ActualWordAfterDelete);
+            Assert.Equal(expectedNrOfWords, actualNrOfWords);
         }
     }
 }
