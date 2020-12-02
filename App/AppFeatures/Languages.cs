ee3ad26de18ffb53994de6949d7d1e038237525a
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-02 11:41:04 +0100

    Initial commit. Contains 4 languages in alphabetical order

---
+++ App/AppFeatures/Languages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFeatures
+{
+    public enum Languages
+    {
+        English,
+        German,
+        Spanish,
+        Swedish,
+    }
+}
