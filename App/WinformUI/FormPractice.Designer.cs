78af4a8e88df45f461cddc3bdbacea300564445d
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-05 23:15:00 +0100

    Gave label for description a better ID than the default

--- App/WinformUI/FormPractice.Designer.cs
+++ App/WinformUI/FormPractice.Designer.cs
@@ -29,25 +29,25 @@ namespace WinformUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // lblDescription
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(295, 29);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Words from list - set on init";
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(27, 27);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(295, 29);
+            this.lblDescription.TabIndex = 0;
+            this.lblDescription.Text = "Words from list - set on init";
             // 
             // FormPractice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDescription);
             this.Name = "FormPractice";
             this.Text = "UpVocabulary - Practice";
             this.ResumeLayout(false);
@@ -57,6 +57,6 @@ namespace WinformUI
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDescription;
     }
 }
