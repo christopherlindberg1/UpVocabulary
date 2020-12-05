e70a681ef5b69b8b959859db7df549745996e67e
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-05 23:14:19 +0100

    Created fields and properties for practice settings

--- App/WinformUI/FormPractice.cs
+++ App/WinformUI/FormPractice.cs
@@ -7,12 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AppFeatures;
 
 namespace WinformUI
 {
     public partial class FormPractice : Form
     {
-        private Queue<string> _lastUsedWords;
+        private readonly InputValidator _inputValidator = new InputValidator();
+        private Vocabulary _vocabulary;
+        private string _originalLanguage;
+        private string _translationLanguage;
+        private int _nrOfWordsToPracticeWith;
+        private bool _promptWithOriginalLanguage;
+        private bool _useLimitedAmountOfWords;
+        private readonly Queue<string> _lastUsedWords = new Queue<string>();
 
 
 
@@ -23,6 +31,91 @@ namespace WinformUI
          * ===================  Properties  ===================
          * 
          */
+
+        private InputValidator InputValidator
+        {
+            get => _inputValidator;
+        }
+
+        private Vocabulary Vocabulary
+        {
+            get => _vocabulary;
+
+            set
+            {
+                _vocabulary = value ??
+                    throw new ArgumentNullException(
+                        "VocabularyToPracticeWith",
+                        "VocabularyToPracticeWith cannot be null.");
+            }
+        }
+
+        public string OriginalLanguage
+        {
+            get => _originalLanguage;
+
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(
+                        "OriginalLanguage",
+                        "OriginalLanguage cannot be null");
+                }
+
+                _originalLanguage = value;
+            }
+        }
+
+        public string TranslationLanguage
+        {
+            get => _translationLanguage;
+
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(
+                        "TranslationLanguage",
+                        "TranslationLanguage cannot be null");
+                }
+
+                _translationLanguage = value;
+            }
+        }
+
+        public int NrOfWordsToPracticeWith
+        {
+            get => _nrOfWordsToPracticeWith;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "NrOfWordsToPracticeWith",
+                        "Must be at least zero");
+                }
+                else
+                {
+                    _nrOfWordsToPracticeWith = value;
+                }
+            }
+        }
+
+        public bool PromptWithOriginalLanguage
+        {
+            get => _promptWithOriginalLanguage;
+
+            set => _promptWithOriginalLanguage = value;
+        }
+
+        public bool UseLimitedAmountOfWords
+        {
+            get => _useLimitedAmountOfWords;
+
+            set => _useLimitedAmountOfWords = value;
+        }
 
         private Queue<string> LastUsedWords
         {
@@ -39,9 +132,39 @@ namespace WinformUI
          * 
          */
 
-        public FormPractice()
+        public FormPractice(
+            Vocabulary vocabulary,
+            string originalLanguage,
+            string translationLanguage,
+            int nrOfWordsToPracticeWith,
+            bool promptWithOriginalLanguage,
+            bool useLimitedAmountOfWords)
         {
             InitializeComponent();
+
+            Vocabulary = vocabulary;
+            OriginalLanguage = originalLanguage;
+            TranslationLanguage = translationLanguage;
+            NrOfWordsToPracticeWith = nrOfWordsToPracticeWith;
+            PromptWithOriginalLanguage = promptWithOriginalLanguage;
+            UseLimitedAmountOfWords = useLimitedAmountOfWords;
+
+            InitializeForm();
+        }
+
+        private void InitializeForm()
+        {
+            InitializeGUI();
+        }
+
+        private void InitializeGUI()
+        {
+
+        }
+
+        private void InitializeDescription()
+        {
+
         }
 
 
