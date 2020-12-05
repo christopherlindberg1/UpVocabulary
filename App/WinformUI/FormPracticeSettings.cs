9986557a031f0df774d89d080c7e57286f109596
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-05 22:24:54 +0100

    Added methods for initializing form to match a vocabulary, methods for validation, and methods for saving practice settings

--- App/WinformUI/FormPracticeSettings.cs
+++ App/WinformUI/FormPracticeSettings.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AppFeatures;
 
 namespace WinformUI
 {
     public partial class FormPracticeSettings : Form
     {
-        private DialogResult _startPractice;
-        private string[] _languagesToPracticeWith = new string[] { "Swedish", "English" };
+        private readonly InputValidator _inputValidator = new InputValidator();
+        private Vocabulary _vocabularyToPracticeWith;
         private int _nrOfWordsToPracticeWith = 0;
+        private bool _promptWithOriginalLanguage;
+        private bool _useLimitedAmountOfWords;
+
 
 
 
@@ -25,37 +29,21 @@ namespace WinformUI
          * 
          */
 
-        public DialogResult StartPractice
+        private InputValidator InputValidator
         {
-            get => _startPractice;
-
-            set
-            {
-                if (value != DialogResult.Yes && value != DialogResult.Cancel)
-                {
-                    throw new ArgumentException("Dialog result must be 'Yes' or 'Cancel'", "StartPractice");
-                }
-                else
-                {
-                    _startPractice = value;
-                }
-            }
+            get => _inputValidator;
         }
 
-        private string[] LanguagesToPracticeWith
+        private Vocabulary VocabularyToPracticeWith
         {
-            get => _languagesToPracticeWith;
+            get => _vocabularyToPracticeWith;
 
             set
             {
-                if (value == null)
-                {
-                    throw new ArgumentNullException("LanguagesToPracticeWith", "Cannot be null");
-                }
-                else
-                {
-                    _languagesToPracticeWith = value;
-                }
+                _vocabularyToPracticeWith = value ??
+                    throw new ArgumentNullException(
+                        "VocabularyToPracticeWith",
+                        "VocabularyToPracticeWith cannot be null.");
             }
         }
 
@@ -78,17 +66,118 @@ namespace WinformUI
             }
         }
 
+        public bool PromptWithOriginalLanguage
+        {
+            get => _promptWithOriginalLanguage;
+
+            set => _promptWithOriginalLanguage = value;
+        }
+
+        public bool UseLimitedAmountOfWords
+        {
+            get => _useLimitedAmountOfWords;
+
+            set => _useLimitedAmountOfWords = value;
+        }
+
+        //public string OriginalLanguage
+        //{
+        //    get => _originalLanguage;
+
+        //    set
+        //    {
+        //        if (String.IsNullOrWhiteSpace(value))
+        //        {
+        //            throw new ArgumentNullException(
+        //                "OriginalLanguage",
+        //                "OriginalLanguage cannot be null");
+        //        }
+
+        //        _originalLanguage = value;
+        //    }
+        //}
+
+        //public string TranslationLanguage
+        //{
+        //    get => _translationLanguage;
+
+        //    set
+        //    {
+        //        if (String.IsNullOrWhiteSpace(value))
+        //        {
+        //            throw new ArgumentNullException(
+        //                "TranslationLanguage",
+        //                "TranslationLanguage cannot be null");
+        //        }
+
+        //        _translationLanguage = value;
+        //    }
+        //}
+
+
+
+
 
         /**
          * 
-         * ===================  Events  ===================
+         * ===================  Init  ===================
          * 
          */
 
-        public FormPracticeSettings()
+        public FormPracticeSettings(Vocabulary vocabularyToPracticeWith)
         {
             InitializeComponent();
+
+            VocabularyToPracticeWith = vocabularyToPracticeWith;
+
+            InitializeForm();
         }
+
+        private void InitializeForm()
+        {
+            InitializeGUI();
+        }
+
+        private void InitializeGUI()
+        {
+            InitializeFormDescription();
+            InitializeLanguageMenu();
+            InitializeNrOfWordsMenu();
+        }
+
+        private void InitializeFormDescription()
+        {
+            lblDescription.Text =
+                $"Configure your practice session for { VocabularyToPracticeWith.Name }";
+        }
+
+        private void InitializeLanguageMenu()
+        {
+            string originalLanguage = VocabularyToPracticeWith.OriginalLanguage;
+            string translationLanguage = VocabularyToPracticeWith.TranslationLanguage;
+
+            comboBoxLanguages.Items.Add($"{ originalLanguage } to { translationLanguage }");
+            comboBoxLanguages.Items.Add($"{ translationLanguage } to { originalLanguage }");
+        }
+
+        private void InitializeNrOfWordsMenu()
+        {
+            int[] choices = (int[])Enum.GetValues(typeof(NrOfWordsToPracticeWith));
+
+            for (int i = 0; i < choices.Length; i++)
+            {
+                if (choices[i] > VocabularyToPracticeWith.NrOfWords)
+                {
+                    break;
+                }
+
+                comboBoxNrOfWords.Items.Add(choices[i]);
+            }
+
+            comboBoxNrOfWords.Items.Add("No limit");
+        }
+
+
 
 
 
@@ -99,9 +188,58 @@ namespace WinformUI
          * 
          */
 
+        private bool ValidateInput()
+        {
+            bool languagesOk = ValidateLanguagesChoice();
+            bool nrOfWordsOk = ValidateNrOfWordsChoice();
+
+            return languagesOk && nrOfWordsOk;
+        }
+
+        private bool ValidateLanguagesChoice()
+        {
+            return InputValidator.ValidateComboBoxItemIsSelected(
+                comboBoxLanguages,
+                "You must choose a language setting.");
+        }
+
+        private bool ValidateNrOfWordsChoice()
+        {
+            return InputValidator.ValidateComboBoxItemIsSelected(
+                comboBoxNrOfWords,
+                "You must choose an amount of words to practice with.");
+        }
+
         private void SavePracticeSettings()
         {
+            SaveLanguageSettings();
+            SaveNrOfWordsToPracticeWith();
+        }
 
+        private void SaveLanguageSettings()
+        {
+            if (comboBoxLanguages.SelectedIndex == 0)
+            {
+                PromptWithOriginalLanguage = true;
+            }
+            else if (comboBoxLanguages.SelectedIndex == 1)
+            {
+                PromptWithOriginalLanguage = false;
+            }
+        }
+
+        private void SaveNrOfWordsToPracticeWith()
+        {
+            string selectedValue = comboBoxNrOfWords.SelectedValue.ToString();
+            if (selectedValue.Equals("No limit"))
+            {
+                UseLimitedAmountOfWords = false;
+            }
+            else
+            {
+                UseLimitedAmountOfWords = true;
+                NrOfWordsToPracticeWith = int.Parse(selectedValue);
+            }
         }
 
 
@@ -116,12 +254,22 @@ namespace WinformUI
 
         private void btnStartPractice_Click(object sender, EventArgs e)
         {
-            this.DialogResult = DialogResult.Yes;
-            
-            // Save practice settings
+            if (ValidateInput() == false)
+            {
+                MessageBox.Show(
+                    InputValidator.GetMessages(),
+                    "Info",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
 
-
-            this.Close();
+                return;
+            }
+            else
+            {
+                SavePracticeSettings();
+                this.DialogResult = DialogResult.Yes;
+                this.Close();
+            }
         }
 
         private void FormPracticeSettings_FormClosing(object sender, FormClosingEventArgs e)
