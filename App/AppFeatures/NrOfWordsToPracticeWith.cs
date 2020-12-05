052fe02413d1753f0e0b56462e88f346858a572a
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-05 22:23:24 +0100

    Initial commit. Contains options for how many words should be generated in a practice session

---
+++ App/AppFeatures/NrOfWordsToPracticeWith.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFeatures
+{
+    public enum NrOfWordsToPracticeWith
+    {
+        First = 5,
+        Second = 10,
+        Third = 25,
+        Fourth = 50
+    }
+}
