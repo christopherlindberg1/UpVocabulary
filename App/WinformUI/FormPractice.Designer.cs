40c63d6d54c72bbe747f470f36e119a25314359e
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-06 00:30:12 +0100

    Added element for showing if user gave correct answer or not

--- App/WinformUI/FormPractice.Designer.cs
+++ App/WinformUI/FormPractice.Designer.cs
@@ -37,6 +37,10 @@ namespace WinformUI
             this.textBoxTranslation = new System.Windows.Forms.TextBox();
             this.groupBoxScore = new System.Windows.Forms.GroupBox();
             this.lblToggleScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNrOfCorrectAnswers = new System.Windows.Forms.Label();
+            this.lblCorrectOrWrong = new System.Windows.Forms.Label();
+            this.groupBoxScore.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblDescription
@@ -62,12 +66,14 @@ namespace WinformUI
             // lblToggleShowSentence
             // 
             this.lblToggleShowSentence.AutoSize = true;
+            this.lblToggleShowSentence.Cursor = System.Windows.Forms.Cursors.Hand;
             this.lblToggleShowSentence.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblToggleShowSentence.Location = new System.Drawing.Point(32, 117);
             this.lblToggleShowSentence.Name = "lblToggleShowSentence";
             this.lblToggleShowSentence.Size = new System.Drawing.Size(247, 20);
             this.lblToggleShowSentence.TabIndex = 2;
             this.lblToggleShowSentence.Text = "Show [word] used in a sentence";
+            this.lblToggleShowSentence.Click += new System.EventHandler(this.lblToggleShowSentence_Click);
             // 
             // lblWordUsedInSentence
             // 
@@ -84,7 +90,7 @@ namespace WinformUI
             // 
             this.lblTranslation.AutoSize = true;
             this.lblTranslation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTranslation.Location = new System.Drawing.Point(51, 204);
+            this.lblTranslation.Location = new System.Drawing.Point(28, 226);
             this.lblTranslation.Name = "lblTranslation";
             this.lblTranslation.Size = new System.Drawing.Size(126, 20);
             this.lblTranslation.TabIndex = 4;
@@ -93,37 +99,71 @@ namespace WinformUI
             // textBoxTranslation
             // 
             this.textBoxTranslation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxTranslation.Location = new System.Drawing.Point(54, 224);
+            this.textBoxTranslation.Location = new System.Drawing.Point(32, 249);
             this.textBoxTranslation.Name = "textBoxTranslation";
-            this.textBoxTranslation.Size = new System.Drawing.Size(133, 27);
+            this.textBoxTranslation.Size = new System.Drawing.Size(247, 27);
             this.textBoxTranslation.TabIndex = 5;
             // 
             // groupBoxScore
             // 
-            this.groupBoxScore.Location = new System.Drawing.Point(442, 78);
+            this.groupBoxScore.Controls.Add(this.lblNrOfCorrectAnswers);
+            this.groupBoxScore.Controls.Add(this.label1);
+            this.groupBoxScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxScore.Location = new System.Drawing.Point(442, 68);
             this.groupBoxScore.Name = "groupBoxScore";
-            this.groupBoxScore.Size = new System.Drawing.Size(310, 204);
+            this.groupBoxScore.Size = new System.Drawing.Size(310, 126);
             this.groupBoxScore.TabIndex = 6;
             this.groupBoxScore.TabStop = false;
-            this.groupBoxScore.Text = "Score";
             // 
             // lblToggleScore
             // 
-            this.lblToggleScore.AutoSize = true;
             this.lblToggleScore.Cursor = System.Windows.Forms.Cursors.Hand;
             this.lblToggleScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblToggleScore.Location = new System.Drawing.Point(708, 55);
+            this.lblToggleScore.Location = new System.Drawing.Point(620, 33);
             this.lblToggleScore.Name = "lblToggleScore";
-            this.lblToggleScore.Size = new System.Drawing.Size(44, 20);
+            this.lblToggleScore.Size = new System.Drawing.Size(132, 23);
             this.lblToggleScore.TabIndex = 7;
-            this.lblToggleScore.Text = "Hide";
+            this.lblToggleScore.Text = "Hide score";
+            this.lblToggleScore.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblToggleScore.Click += new System.EventHandler(this.lblToggleScore_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score";
+            // 
+            // lblNrOfCorrectAnswers
+            // 
+            this.lblNrOfCorrectAnswers.AutoSize = true;
+            this.lblNrOfCorrectAnswers.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNrOfCorrectAnswers.Location = new System.Drawing.Point(22, 65);
+            this.lblNrOfCorrectAnswers.Name = "lblNrOfCorrectAnswers";
+            this.lblNrOfCorrectAnswers.Size = new System.Drawing.Size(178, 36);
+            this.lblNrOfCorrectAnswers.TabIndex = 1;
+            this.lblNrOfCorrectAnswers.Text = "0 / 0 Correct";
+            // 
+            // lblCorrectOrWrong
+            // 
+            this.lblCorrectOrWrong.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorrectOrWrong.Location = new System.Drawing.Point(27, 303);
+            this.lblCorrectOrWrong.Name = "lblCorrectOrWrong";
+            this.lblCorrectOrWrong.Size = new System.Drawing.Size(252, 23);
+            this.lblCorrectOrWrong.TabIndex = 8;
+            this.lblCorrectOrWrong.Text = "[Cleared on init]";
+            this.lblCorrectOrWrong.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormPractice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 402);
+            this.Controls.Add(this.lblCorrectOrWrong);
             this.Controls.Add(this.lblToggleScore);
             this.Controls.Add(this.groupBoxScore);
             this.Controls.Add(this.textBoxTranslation);
@@ -134,6 +174,8 @@ namespace WinformUI
             this.Controls.Add(this.lblDescription);
             this.Name = "FormPractice";
             this.Text = "UpVocabulary - Practice";
+            this.groupBoxScore.ResumeLayout(false);
+            this.groupBoxScore.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,5 +191,8 @@ namespace WinformUI
         private System.Windows.Forms.TextBox textBoxTranslation;
         private System.Windows.Forms.GroupBox groupBoxScore;
         private System.Windows.Forms.Label lblToggleScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNrOfCorrectAnswers;
+        private System.Windows.Forms.Label lblCorrectOrWrong;
     }
 }
