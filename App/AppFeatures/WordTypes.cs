74d6f4e40f4e4476d8290ebda55b03218c46d22e
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-11 14:54:14 +0100

    Enum for different word classes

---
+++ App/AppFeatures/WordTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFeatures
+{
+    public enum WordTypes
+    {
+        Noun,
+        Verb,
+        Adjective,
+        Adverb
+    }
+}
