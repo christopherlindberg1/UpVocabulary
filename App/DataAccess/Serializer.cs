d516b40e1867aef7d15c65c95be9eddec161665d
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-11 12:09:40 +0100

    File for storing data in the VocabularyManager

---
+++ App/DataAccess/Serializer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using AppFeatures;
+
+namespace DataAccess
+{
+    public class Serializer
+    {
+        private static string _dataFolderPath;
+
+
+
+
+
+        /**
+         * 
+         * ===================  Properties  ===================
+         * 
+         */
+
+        private static string DataFolderPath
+        {
+            get => _dataFolderPath;
+
+            set
+            {
+                _dataFolderPath = value ??
+                    throw new ArgumentNullException(
+                        "DataFolderPath",
+                        "DataFolderPath cannot be null.");
+            }
+        }
+
+        public static void SaveVocabularyManager(VocabularyManager vocabularyManager)
+        {
+            string storageFolderPath = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\App\DataAccess\Data\");
+            string filePath = Path.Combine(storageFolderPath, @".\VocabularyManager.xml");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(VocabularyManager));
+
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                serializer.Serialize(sw, vocabularyManager);
+            }
+        }
+    }
+}
