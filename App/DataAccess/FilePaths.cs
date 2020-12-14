cd4d9c07c718b6960d47283978d02ed8e0ab2e9a
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-14 15:29:38 +0100

    Initial commit. Class with file paths for easy access

---
+++ App/DataAccess/FilePaths.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DataAccess
+{
+    public abstract class FilePaths
+    {
+        private static string DataStorageFolder
+        {
+            get
+            {
+                return Path.GetFullPath(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\DataAccess\Data"));
+            }
+        }
+
+        public static string VocabularyManagerFilePath
+        {
+            get
+            {
+                return Path.GetFullPath(
+                    Path.Combine(DataStorageFolder, @".\VocabularyManager.xml"));
+            }
+        }
+    }
+}
