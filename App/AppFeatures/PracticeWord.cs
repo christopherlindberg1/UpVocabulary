fc0da2f427f14bf4b458c0f5bfc76e540068b826
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-14 15:30:28 +0100

    Helper class / object to keep track of data during a practice session

---
+++ App/AppFeatures/PracticeWord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFeatures
+{
+    public class PracticeWord
+    {
+        public Word Word { get; set; }
+        public string UserTranslation { get; set; }
+        public bool CorrectTranslation { get; set; }
+
+        public PracticeWord(Word word, string userTranslation, bool correctTranslation)
+        {
+            Word = word;
+            UserTranslation = userTranslation;
+            CorrectTranslation = correctTranslation;
+        }
+    }
+}
