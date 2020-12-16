ae1f0c1547ed88ac999062548eb0abf51ad5c374
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-16 18:04:34 +0100

    Added fields and properties for 4 settings that the user should be able to control

--- App/AppFeatures/AppSettings.cs
+++ App/AppFeatures/AppSettings.cs
@@ -10,6 +10,84 @@ namespace AppFeatures
     [Serializable()]
     public class AppSettings : ISerializable
     {
+        private AppLanguages _appLanguage;
+        private bool _autoPromptQuestionAfterCorrectAnswer;
+        private bool _autoPromptQuestionAfterIncorrectAnswer;
+        private int _delayBeforePromptingNextQuestion;
+
+
+
+
+
+        /**
+         * 
+         * ===================  Properties  ===================
+         * 
+         */
+
+        /// <summary>
+        ///   Language for all text in the application, except the words used for
+        ///   the practice itself.
+        /// </summary>
+        public AppLanguages AppLanguage
+        {
+            get => _appLanguage;
+
+            set => _appLanguage = value;
+        }
+
+        /// <summary>
+        ///   Determines whether the next question should be prompted automaticlly
+        ///   after the user has given a correct translation.
+        /// </summary>
+        public bool AutoPromptQuestionAfterCorrectAnswer
+        {
+            get => _autoPromptQuestionAfterCorrectAnswer;
+
+            set => _autoPromptQuestionAfterCorrectAnswer = value;
+        }
+
+        /// <summary>
+        ///   Determines whether the next question should be prompted automaticlly
+        ///   after the user has given an icorrect translation.
+        /// </summary>
+        public bool AutoPromptQuestionAfterIncorrectAnswer
+        {
+            get => _autoPromptQuestionAfterIncorrectAnswer;
+
+            set => _autoPromptQuestionAfterIncorrectAnswer = value;
+        }
+
+        /// <summary>
+        ///   Delay in milliseconds for prompting the next question
+        ///   to the user.
+        /// </summary>
+        public int DelayBeforePromptingNextQuestion
+        {
+            get => _delayBeforePromptingNextQuestion;
+
+            set
+            {
+                if (value < 0 || value > 30000)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "DelayBeforePromptingNextQuestion",
+                        "DelayBeforePromptingNextQuestion must be in the range 0 - 30000.");
+                }
+
+                _delayBeforePromptingNextQuestion = value;
+            }
+        }
+
+
+
+
+
+        /**
+         * 
+         * ===================  Methods  ===================
+         * 
+         */
         public void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             throw new NotImplementedException();
