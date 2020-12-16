085733b90ed658096c0488710f46c96fa9bbc81b
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-16 17:47:06 +0100

    Initial commit

---
+++ App/AppFeatures/AppSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+
+
+namespace AppFeatures
+{
+    [Serializable()]
+    public class AppSettings : ISerializable
+    {
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
