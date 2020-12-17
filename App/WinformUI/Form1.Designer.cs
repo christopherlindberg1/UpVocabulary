fdfd5b898779451e008a920e79ed05a143eb282a
christopherlindberg1 <christopher.lindberg1@gmail.com>
2020-12-17 14:18:24 +0100

    Initial commit for alternative design

---
+++ App/WinformUI/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace WinformUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelNavigation = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblUpVocabulary = new System.Windows.Forms.Label();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panelMainContent = new System.Windows.Forms.Panel();
+            this.panelMenuChoiceIndicator = new System.Windows.Forms.Panel();
+            this.panelNavigation.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelNavigation
+            // 
+            this.panelNavigation.BackColor = System.Drawing.Color.White;
+            this.panelNavigation.Controls.Add(this.btnSettings);
+            this.panelNavigation.Controls.Add(this.panelMenuChoiceIndicator);
+            this.panelNavigation.Controls.Add(this.button2);
+            this.panelNavigation.Controls.Add(this.button1);
+            this.panelNavigation.Controls.Add(this.panel2);
+            this.panelNavigation.Location = new System.Drawing.Point(0, 0);
+            this.panelNavigation.Name = "panelNavigation";
+            this.panelNavigation.Size = new System.Drawing.Size(225, 613);
+            this.panelNavigation.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel2.Controls.Add(this.lblUpVocabulary);
+            this.panel2.ForeColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(225, 135);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblUpVocabulary
+            // 
+            this.lblUpVocabulary.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpVocabulary.Location = new System.Drawing.Point(0, 0);
+            this.lblUpVocabulary.Name = "lblUpVocabulary";
+            this.lblUpVocabulary.Size = new System.Drawing.Size(225, 135);
+            this.lblUpVocabulary.TabIndex = 0;
+            this.lblUpVocabulary.Text = "UpVocabulary";
+            this.lblUpVocabulary.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSettings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.btnSettings.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(255)))));
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Image = ((System.Drawing.Image)(resources.GetObject("btnSettings.Image")));
+            this.btnSettings.Location = new System.Drawing.Point(0, 405);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(225, 120);
+            this.btnSettings.TabIndex = 0;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSettings.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(0, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 120);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "My Vocabularies";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(255)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(0, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 120);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Add Vocabulary";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panelMainContent
+            // 
+            this.panelMainContent.BackColor = System.Drawing.Color.White;
+            this.panelMainContent.Location = new System.Drawing.Point(225, 0);
+            this.panelMainContent.Name = "panelMainContent";
+            this.panelMainContent.Size = new System.Drawing.Size(825, 650);
+            this.panelMainContent.TabIndex = 1;
+            // 
+            // panelMenuChoiceIndicator
+            // 
+            this.panelMenuChoiceIndicator.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panelMenuChoiceIndicator.Location = new System.Drawing.Point(215, 155);
+            this.panelMenuChoiceIndicator.Name = "panelMenuChoiceIndicator";
+            this.panelMenuChoiceIndicator.Size = new System.Drawing.Size(10, 80);
+            this.panelMenuChoiceIndicator.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1032, 603);
+            this.Controls.Add(this.panelMainContent);
+            this.Controls.Add(this.panelNavigation);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UpVocabulary";
+            this.panelNavigation.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelNavigation;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Label lblUpVocabulary;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelMainContent;
+        private System.Windows.Forms.Panel panelMenuChoiceIndicator;
+    }
+}
