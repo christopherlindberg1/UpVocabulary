587af3021b4dce705a36b0d9836062fa877625ca
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-01-21 19:13:47 +0100

    initial commit. Added asc and desc as sorting directions

---
+++ App/AppFeatures/SortingDirections.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFeatures
+{
+    /// <summary>
+    ///   Enum with the possible sorting directions for comparable items.
+    /// </summary>
+    public enum SortingDirections
+    {
+        Asc,
+        Desc,
+    }
+}
