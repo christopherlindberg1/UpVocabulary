64ee7a5a2fe3497ccba6150de71834f03066d51d
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-01-21 23:35:42 +0100

    Initial commit

---
+++ App/AppFeatures/SortingClasses/SortVocabularyByTranslationLanguage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFeatures.SortingClasses
+{
+    public class SortVocabularyByTranslationLanguage : IComparer<Vocabulary>
+    {
+        public int Compare(Vocabulary x, Vocabulary y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
