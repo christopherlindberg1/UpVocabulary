e0799a9249b0d619e8aafa2ad44c2ca33db272ea
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-01-22 19:40:52 +0100

    Map for how languages is translated into other languages

---
+++ App/AppFeatures/LanguagesTranslationMap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFeatures
+{
+    public static class LanguagesTranslationMap
+    {
+        private static Dictionary<string, Dictionary<AppLanguages, string>> _map = new Dictionary<string, Dictionary<AppLanguages, string>>
+        {
+            { 
+                "albanian", new Dictionary<AppLanguages, string> {
+                    { AppLanguages.English, "albanian" },
+                    { AppLanguages.Swedish, "albanska" }}
+            },
+            { 
+                "english", new Dictionary<AppLanguages, string> {
+                    { AppLanguages.English, "english" },
+                    { AppLanguages.Swedish, "engelska" }
+                }
+            },
+            {
+                "german", new Dictionary<AppLanguages, string> {
+                    { AppLanguages.English, "german" },
+                    { AppLanguages.Swedish, "tyska" }
+                }
+            },
+            {
+                "spanish", new Dictionary<AppLanguages, string> {
+                    { AppLanguages.English, "spanish" },
+                    { AppLanguages.Swedish, "spanska" }
+                }
+            },
+            {
+                "swedish", new Dictionary<AppLanguages, string> {
+                    { AppLanguages.English, "swedish" },
+                    { AppLanguages.Swedish, "svenska" }
+                }
+            }
+        };
+
+
+        public static Dictionary<string, Dictionary<AppLanguages, string>> Map
+        {
+            get => _map;
+        }
+    }
+}
