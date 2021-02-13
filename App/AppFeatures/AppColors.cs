acda4d30ce24f24b48ae73daf72491ff9be5605c
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-02-13 13:38:31 +0100

    Class containing colors used in the app

---
+++ App/AppFeatures/AppColors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AppFeatures
+{
+    public class AppColors
+    {
+        public static Color BtnBlue { get; } = Color.DodgerBlue;
+        public static Color BtnBlueHover { get; } = Color.RoyalBlue;
+        public static Color BtnGray { get; } = Color.FromArgb(190, 190, 190);
+        public static Color BtnGrayHover { get; } = Color.FromArgb(170, 170, 170);
+        public static Color BtnRed { get; } = Color.FromArgb(240, 0, 0);
+        public static Color BtnRedHover { get; } = Color.FromArgb(200, 0, 0);
+    }
+}
